41372e5db4b0b775802f2c3c9aaabda5abd3146a
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-04-29 20:46:03 -0300

    Sequelize: Criação do model Users e sua respectiva migration

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,44 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+export default class Users extends Model {
+  public id:number;
+
+  public username:string;
+
+  public role:string;
+
+  public email:string;
+
+  public password:string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
