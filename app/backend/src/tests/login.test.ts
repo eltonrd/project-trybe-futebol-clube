d9bbd0dc14b12b4dca2018d5ed4b9d5968d22fad
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-05-02 19:20:52 -0300

    TDD: Testes para a rota /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,4 +1,3 @@
-import * as sinon from 'sinon';
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
@@ -17,8 +16,37 @@ describe('Testa a rota /login', () => {
     password: 'test123456'
   }
 
+  const requestFail = {
+    email: 'invalidEmail',
+    password: '1234'
+  }
+  const requestFail2 = {
+    email: '',
+    password: 'test123456'
+  }
+  const requestFail3 = {
+    email: 'test@test.com',
+    password: ''
+  }
+
   it('A rota /login deve retornar um status 200', async () => {
     chaiHttpResponse = await chai.request(app).post('/login').send(requestOk);
     expect(chaiHttpResponse).to.have.status(200);
+  });
+
+  it('Os campos email e password devem ser válidos', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login').send(requestFail);
+    expect(chaiHttpResponse.body.error).to.be.equal('Incorrect email or password');
+    expect(chaiHttpResponse).to.have.status(400);
+  });
+  it('O campo email deve ser válido', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login').send(requestFail2);
+    expect(chaiHttpResponse.body.error).to.be.equal('All fields must be filled');
+    expect(chaiHttpResponse).to.have.status(400);
+  });
+  it('O campo password deve ser válido', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login').send(requestFail3);
+    expect(chaiHttpResponse.body.error).to.be.equal('All fields must be filled');
+    expect(chaiHttpResponse).to.have.status(400);
   });
 });
